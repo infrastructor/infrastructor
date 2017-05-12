7d3aefa66f72b0b9ba33a61553702c20984170b0
Stanislav Tyurikov <stanislav.tyurikov@gmail.com>
2017-05-12 23:46:18 +0200

    add some tests

--- src/test/java/io/infrastructor/core/tasks/ReplaceLineActionTest.groovy
+++ src/test/java/io/infrastructor/core/tasks/ReplaceLineActionTest.groovy
@@ -32,5 +32,60 @@ public class ReplaceLineActionTest extends TaskTestBase {
             }
         }
     }
+    
+    @Test
+    public void replaceLineWithoutChange() {
+        inventory.setup {
+            nodes('as:root') {
+                file {
+                    target  = '/test.txt'
+                    content = """\
+                    line 1
+                    line 2
+
+                    line 3
+                    """
+                }
+                
+                replaceLine {
+                    target = '/test.txt'
+                    regexp = "^unknown"
+                    line   = "unknown"
+                }       
+                
+                assert shell("cat /test.txt").output == """\
+                line 1
+                line 2
+
+                line 3""".stripMargin().stripIndent()
+            }
+        }
+    }
+    
+    @Test
+    public void replaceLineChangeAttributes() {
+        inventory.setup {
+            nodes('as:root') {
+                file {
+                    target  = '/test.txt'
+                    content = "dummy content"
+                }
+                
+                replaceLine {
+                    target = '/test.txt'
+                    regexp = "^unknown"
+                    line   = "unknown"
+                    owner  = "devops"  // user  devops already exists
+                    group  = "devops"  // group devops already exists
+                    mode   = '600'
+                }       
+                
+                def result = shell("ls -dalh /test.txt")
+                assert result.output.contains("test.txt")
+                assert result.output.contains("devops devops")
+                assert result.output.contains("-rw------")
+            }
+        }
+    }
 }
 
