62518dac0649e36025f4a9136823221975cc325a
Stanislav Tyurikov <stanislav.tyurikov@gmail.com>
2017-05-25 19:26:40 +0200

    add template encryption and decryption tests

--- src/test/java/io/infrastructor/cli/handlers/DecryptHandlerTest.groovy
+++ src/test/java/io/infrastructor/cli/handlers/DecryptHandlerTest.groovy
@@ -5,10 +5,19 @@ import org.testng.annotations.Test
 public class DecryptHandlerTest {
     @Test
     public void decryptFile() {
-        def file = 'build/resources/test/decryption.txt'
+        def file = 'build/resources/test/decryption/file.txt'
         DecryptHandler handler = new DecryptHandler(files: [file], password: 'test')
         handler.execute()
         assert "simple\ntest\n" == new File(file).text
+    }
+    
+    @Test
+    public void decryptTemplate() {
+        def file = 'build/resources/test/decryption/template.txt'
+        DecryptHandler handler = new DecryptHandler(templates: [file], password: 'test')
+        handler.execute()
+        def text = new File(file).text
+        assert "message: simple\ntest\n" == text
     }
 }
 
--- src/test/java/io/infrastructor/cli/handlers/EncryptHandlerTest.groovy
+++ src/test/java/io/infrastructor/cli/handlers/EncryptHandlerTest.groovy
@@ -5,10 +5,18 @@ import org.testng.annotations.Test
 public class EncryptHandlerTest {
     @Test
     public void encryptFile() {
-        def file = 'build/resources/test/encryption.txt'
+        def file = 'build/resources/test/encryption/file.txt'
         EncryptHandler handler = new EncryptHandler(files: [file], password: 'test')
         handler.execute()
         assert "d69wE24VdwcW3dlrFrEDvg==" == new File(file).text
+    }
+    
+    @Test
+    public void encryptTemplate() {
+        def file = 'build/resources/test/encryption/template.txt'
+        EncryptHandler handler = new EncryptHandler(templates: [file], password: 'test')
+        handler.execute()
+        assert 'message: ${decrypt(\'d69wE24VdwcW3dlrFrEDvg==\')}' == new File(file).text
     }
 }
 
