0aa245af38828b62cf34ffba7ccb56817e76b2d7
Stanislav Tyurikov <stanislav.tyurikov@gmail.com>
2017-05-27 22:01:20 +0200

    add console output

--- src/main/java/io/infrastructor/cli/handlers/EncryptHandler.groovy
+++ src/main/java/io/infrastructor/cli/handlers/EncryptHandler.groovy
@@ -1,23 +1,37 @@
 package io.infrastructor.cli.handlers
 
 import com.beust.jcommander.Parameter
-import io.infrastructor.cli.ConsoleLogger
 import io.infrastructor.cli.validation.FileValidator
 import io.infrastructor.cli.validation.ModeValidator
 import io.infrastructor.core.utils.CryptoUtils
 
+import static io.infrastructor.cli.ConsoleLogger.*
 
-public class EncryptHandler {
+
+public class EncryptHandler extends LoggingAwareHandler {
     
     @Parameter(names = ["-p", "--password"], required = true)
     String password
 
-    @Parameter(names = ["-f", "--file"], validateWith = FileValidator)
+    @Parameter(names = ["-f", "--file"], required = true, validateWith = FileValidator)
     def files = []
     
     @Parameter(names = ["-m", "--mode"], validateWith = ModeValidator)
     String mode = 'FULL'
 
+    
+    def description() {
+        "Encrypt specified files (AES + Base64)."
+    }
+
+    
+    def options() {
+        def options = super.options() 
+        options << ["--file, -f" : "A file to encrypt. This file will be replaced with an encrypted one."]
+        options << ["--mode, -m" : "Encryption mode: FULL or PART. Full mode to encrypt entire file. Part mode to substitute 'encrypt' placeholders only."]
+        options << ["--password, -p" : "An encryption key."]
+    }
+    
     
     def usage() {
         ["infrastructor encrypt -f FILE1 -f FILE2 -p SECRET -m FULL", 
@@ -25,37 +39,27 @@ public class EncryptHandler {
     }
     
     
-    def options() {
-        ["--file, -f" : "A file to encrypt. This file will be replaced with an encrypted one.",
-         "--mode, -m" : "Encryption mode: FULL or PART. Full mode to encrypt entire file. Part mode to substitute 'encrypt' placeholders only.",
-         "--password, -p" : "An encryption key."]
-    }
-    
-    
-    def description() {
-        "Encrypt specified files (AES + Base64)."
-    }
-    
-    
     def execute() {
-        encryptFiles(files.collect { new File(it) }) 
+        super.execute()
+        info "${blue('starting encryption with mode ' + mode)}"
+        encryptFiles( files?.collect { new File(it) } )  
     }
     
     
     def encryptFiles(def files) {
-        files?.each { file ->
-            file.isDirectory() ? encryptFiles(file.listFiles()) : encryptFile(file)
-        }
+        files?.each { it.isDirectory() ? encryptFiles(it.listFiles()) : encryptFile(it) }
     }
     
     
     def encryptFile(def file) {
         def encrypted = (mode == 'FULL') ?
-        CryptoUtils.encryptFullBytes(password, file.getBytes(), 80) :
-        CryptoUtils.encryptPart(password, file.getText(), 80)
+            CryptoUtils.encryptFullBytes(password, file.getBytes(), 80) :
+            CryptoUtils.encryptPart(password, file.getText(), 80)
         def output = new FileOutputStream(file, false)
         output.withCloseable { out ->
             out << encrypted
         }
+        
+        info "${green('encrypted')}: ${file.getCanonicalPath()}"
     }
 }
--- src/main/java/io/infrastructor/cli/handlers/RunHandler.groovy
+++ src/main/java/io/infrastructor/cli/handlers/RunHandler.groovy
@@ -2,47 +2,42 @@ package io.infrastructor.cli.handlers
 
 import com.beust.jcommander.DynamicParameter
 import com.beust.jcommander.Parameter
-import io.infrastructor.cli.ConsoleLogger
 import io.infrastructor.cli.validation.FileValidator
-import io.infrastructor.cli.validation.LogLevelValidator
 import org.codehaus.groovy.control.CompilationFailedException
 import org.codehaus.groovy.control.CompilerConfiguration
 import org.codehaus.groovy.control.customizers.ImportCustomizer
 
-import static io.infrastructor.cli.ApplicationProperties.LOG_LEVEL
-import static java.lang.String.valueOf
 
-
-public class RunHandler {
+public class RunHandler extends LoggingAwareHandler {
     
     @DynamicParameter(names = ["-v", "-V", "--variable"])
     Map variables = [:]
 
     @Parameter(names = ["-f", "--file"], required = true, validateWith = FileValidator)
     def file
-
-    @Parameter(names = ["-l", "--log"], validateWith = LogLevelValidator)
-    int logLevel = ConsoleLogger.INFO
-    
-    def usage() {
-        ["infrastructor run -f FILE -v PROPERTY_X=VALUE_X -v PROPERTY_Y=VALUE_Y -l LOGLEVEL", 
-        "infrastructor run --file FILE -v PROPERTY_X=VALUE_X -v PROPERTY_Y=VALUE_Y --log LOGLEVEL"]
-    }
-    
-    def options() {
-        ["--file, -f" : "File to run.",
-         "--variable, -v" : "Define a runtime variable.",
-         "--log, -l" : "Specify a log level: 0 - OFF, 1 - ERROR, 2 - INFO, 3 - DEBUG"]
-    }
     
     def description() {
         "Run specified file."
     }
     
+    
+    def options() {
+        def options = super.options() 
+        options << ["--file, -f" : "File to run."]
+        options << ["--variable, -v" : "Define a runtime variable."]
+    }
+    
+    
+    def usage() {
+        ["infrastructor run -f FILE -v PROPERTY_X=VALUE_X -v PROPERTY_Y=VALUE_Y -l LOGLEVEL", 
+         "infrastructor run --file FILE -v PROPERTY_X=VALUE_X -v PROPERTY_Y=VALUE_Y --log LOGLEVEL"]
+    }
+    
+    
     def execute() {
+        super.execute()
+        
         try {
-            System.setProperty(LOG_LEVEL, valueOf(logLevel))
-                
             ImportCustomizer importCustomizer = new ImportCustomizer()
             importCustomizer.addStaticStars("io.infrastructor.cli.ConsoleLogger")
             importCustomizer.addStaticStars("io.infrastructor.core.inventory.InlineInventory")
