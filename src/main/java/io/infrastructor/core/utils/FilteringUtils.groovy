100edcefc98a0568d649602b680a95cbb09cffd7
Stanislav Tyurikov <stanislav.tyurikov@gmail.com>
2017-06-07 00:01:15 +0200

    add FilteringUtils

---
+++ src/main/java/io/infrastructor/core/utils/FilteringUtils.groovy
@@ -0,0 +1,14 @@
+package io.infrastructor.core.utils
+
+public class FilteringUtils {
+
+    def static match(def tags, def filter) {
+        def proxy = ProxyMetaClass.getInstance(String.class)
+        proxy.use {
+            def closure = { context -> context.contains(delegate) }
+            String.metaClass.asBoolean = closure.curry(tags)
+            return (filter() as Boolean)
+        }
+    }
+}
+
