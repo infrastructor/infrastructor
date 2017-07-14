790a9f8a5d906dcb6372ad041815da60b96ba67b
Stanislav Tyurikov <stanislav.tyurikov@gmail.com>
2017-07-14 16:28:20 +0200

    add some tests for CLI parameter validators

---
+++ infrastructor-cli/src/test/java/io/infrastructor/cli/validation/LogLevelValidatorTest.groovy
@@ -0,0 +1,28 @@
+package io.infrastructor.cli.validation
+
+import com.beust.jcommander.ParameterException
+import org.junit.Test
+
+class LogLevelValidatorTest {
+    @Test
+    public void passCorrectLogLevelAsInteger() {
+        LogLevelValidator validator = new LogLevelValidator()
+        validator.validate("", "0")
+        validator.validate("", "1")
+        validator.validate("", "2")
+        validator.validate("", "3")
+    }
+    
+    @Test(expected = ParameterException)
+    public void passIncorrectLogLevelAsInteger_minus_one() {
+        LogLevelValidator validator = new LogLevelValidator()
+        validator.validate("", "-1")
+    }
+    
+    @Test(expected = ParameterException)
+    public void passIncorrectLogLevelAsInteger_four() {
+        LogLevelValidator validator = new LogLevelValidator()
+        validator.validate("", "4")
+    }
+}
+
---
+++ infrastructor-cli/src/test/java/io/infrastructor/cli/validation/ModeValidationTest.groovy
@@ -0,0 +1,36 @@
+package io.infrastructor.cli.validation
+
+import com.beust.jcommander.ParameterException
+import org.junit.Test
+
+class ModeValidationTest {
+    @Test
+    public void passCorrectMode() {
+        ModeValidator validator = new ModeValidator()
+        validator.validate("", "FULL")
+        validator.validate("", "PART")
+        validator.validate("", "full")
+        validator.validate("", "fuLL")
+        validator.validate("", "PART")
+        validator.validate("", "PArt")
+    }
+    
+    @Test(expected = ParameterException)
+    public void passIncorrectMode_empty() {
+        ModeValidator validator = new ModeValidator()
+        validator.validate("", "")
+    }
+    
+    @Test(expected = ParameterException)
+    public void passIncorrectMode_null() {
+        ModeValidator validator = new ModeValidator()
+        validator.validate("", "")
+    }
+    
+    @Test(expected = ParameterException)
+    public void passIncorrectMode_something() {
+        ModeValidator validator = new ModeValidator()
+        validator.validate("", "dummy")
+    }
+}
+
