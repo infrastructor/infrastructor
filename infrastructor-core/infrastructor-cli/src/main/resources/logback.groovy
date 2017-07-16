733ea7404e17ab26e3f78103a6d2d454744b8eaa
Stanislav Tyurikov <stanislav.tyurikov@gmail.com>
2017-07-16 22:33:33 +0200

    remove duplicated files

--- infrastructor-core/infrastructor-cli/src/main/resources/logback.groovy
+++
@@ -1,8 +0,0 @@
-appender("CONSOLEGREEN", ConsoleAppender) {
-    encoder(PatternLayoutEncoder) {
-        // %d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n
-        pattern = "%d{HH:mm:ss.SSS} - %highlight(%msg%n)"
-    }
-}
-
-root(OFF, ["CONSOLEGREEN"])
