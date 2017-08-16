0c93365d793bb7179b7e7e44e3434b04a0552858
Stanislav Tiurikov <stanislav.tiurikov@gmail.com>
2017-08-16 22:42:56 +0200

    add multiline shell actions support: shell commands will be written to a temp file first then executed with sh interpreter

--- infrastructor-core/src/main/java/io/infrastructor/core/processing/actions/ShellAction.groovy
+++ infrastructor-core/src/main/java/io/infrastructor/core/processing/actions/ShellAction.groovy
@@ -8,7 +8,15 @@ class ShellAction {
     def sudo
     
     def execute(def node) {
-        node.execute(command: command, sudo: sudo)
+        if (command.contains("\n")) {
+            def result = node.execute(command: "mktemp")
+            def temp = result.output
+            node.writeText(temp, command.stripIndent())
+            node.execute(command: "sh $temp", sudo: sudo)
+        } else {
+            node.execute(command: command, sudo: sudo)
+        }
+        
         node.lastResult
     }
 }
--- infrastructor-core/src/test/java/io/infrastructor/core/processing/actions/ShellActionTest.groovy
+++ infrastructor-core/src/test/java/io/infrastructor/core/processing/actions/ShellActionTest.groovy
@@ -5,13 +5,64 @@ import org.junit.Test
 public class ShellActionTest extends ActionTestBase {
 
     @Test
-    public void simpleShellAction() {
+    public void singleLineShellAction() {
         inventory.provision {
             task name: 'simpleShellAction', filter: {'as:devops'}, actions: {
                 def result = shell { 
                     command = "echo 'simple message!'"
                 }
                 assert result.output.contains('simple message!')
+            }
+        }
+    }
+    
+    
+    @Test
+    public void multilineShellAction() {
+        inventory.provision {
+            task name: 'simpleShellAction', filter: {'as:devops'}, actions: {
+                def result = shell { 
+                    command = """ 
+                        echo 'simple message!'
+                        echo 'another message!'
+                    """
+                }
+                
+                assert result.output.contains('simple message!')
+                assert result.output.contains('another message!')
+            }
+        }
+    }
+    
+    
+    @Test
+    public void multilineRestrictedShellActionWithoutSudo() {
+        inventory.provision {
+            task name: 'simpleShellAction', filter: {'as:devops'}, actions: {
+                def result = shell { 
+                    command = """ 
+                        mkdir /etc/test
+                    """
+                }
+                
+                assert result.exitcode == 1
+                assert result.error.contains("Permission denied")
+            }
+        }
+    }
+    
+    @Test
+    public void multilineRestrictedShellActionWithSudo() {
+        inventory.provision {
+            task name: 'simpleShellAction', filter: {'as:devops'}, actions: {
+                def result = shell { 
+                    command = """ 
+                        mkdir /etc/test
+                    """
+                    sudo = true
+                }
+                
+                assert result.exitcode == 0
             }
         }
     }
