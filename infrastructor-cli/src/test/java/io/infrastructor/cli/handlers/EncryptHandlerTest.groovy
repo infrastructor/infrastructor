f38e7744e13b6694dd657120761cc0a0f83f1df6
Stanislav Tiurikov <stanislav.tiurikov@payworks.com>
2018-06-25 09:28:14 +0200

    remove unused code

--- infrastructor-cli/src/test/java/io/infrastructor/cli/handlers/EncryptHandlerTest.groovy
+++ infrastructor-cli/src/test/java/io/infrastructor/cli/handlers/EncryptHandlerTest.groovy
@@ -9,7 +9,6 @@ class EncryptHandlerTest {
         def file = 'build/resources/test/encryption/file.txt'
         EncryptHandler handler = new EncryptHandler(files: [file], password: 'test')
         handler.execute()
-        println (new File(file).text)
         assert new File(file).text.contains('INFRASTRUCTOR:AES/GCM/PKCS5Padding:BASE64:lU1aSf1w2bi82zXSUiZ4KZV/fvf6bHT4hBm9xegiCfQ=:')
     }
     
