e0043755a0d3dec644cfe41f9ad23b1942d88eb8
tiurikov <stanislav.tyurikov@gmail.com>
2018-07-14 14:34:45 +0200

    update replaceLine  action tests

--- infrastructor-core/src/test/java/io/infrastructor/core/provisioning/actions/ReplaceLineActionTest.groovy
+++ infrastructor-core/src/test/java/io/infrastructor/core/provisioning/actions/ReplaceLineActionTest.groovy
@@ -1,12 +1,13 @@
 package io.infrastructor.core.provisioning.actions
 
 import io.infrastructor.core.inventory.InventoryAwareTestBase
+import io.infrastructor.core.provisioning.TaskExecutionException
 import org.junit.Test
 
 class ReplaceLineActionTest extends InventoryAwareTestBase {
 
     @Test
-    void replaceLineInFile() {
+    void "replace lines in a file"() {
         withUser(DEVOPS) { inventory ->
             inventory.provision {
                 task actions: {
@@ -39,7 +40,78 @@ class ReplaceLineActionTest extends InventoryAwareTestBase {
     }
 
     @Test
-    void replaceLineWithoutChange() {
+    void "replace lines in a file with sudo and a password"() {
+        withUser(SUDOPS) { inventory ->
+            inventory.provision {
+                task actions: {
+                    file {
+                        user = 'root'
+                        target = '/test.txt'
+                        content = """\
+                        line 1
+                        line 2
+    
+                        line 3
+                        """
+                        sudopass = 'sudops'
+                    }
+
+                    replaceLine {
+                        user = 'root'
+                        target = '/test.txt'
+                        regexp = "^line 2"
+                        line = "new line"
+                        sudopass = 'sudops'
+                    }
+
+                    assert shell("cat /test.txt").output == """\
+                    line 1
+                    new line
+    
+                    line 3""".stripMargin().stripIndent()
+                }
+            }
+        }
+    }
+
+
+    @Test(expected = TaskExecutionException)
+    void "replace lines in a file with sudo and a wrong password"() {
+        withUser(SUDOPS) { inventory ->
+            inventory.provision {
+                task actions: {
+                    file {
+                        user = 'root'
+                        target = '/test.txt'
+                        content = """\
+                        line 1
+                        line 2
+    
+                        line 3
+                        """
+                        sudopass = 'sudops'
+                    }
+
+                    replaceLine {
+                        user = 'root'
+                        target = '/test.txt'
+                        regexp = "^line 2"
+                        line = "new line"
+                        sudopass = 'wrong'
+                    }
+
+                    assert shell("cat /test.txt").output == """\
+                    line 1
+                    new line
+    
+                    line 3""".stripMargin().stripIndent()
+                }
+            }
+        }
+    }
+
+    @Test
+    void "replace a line with no change"() {
         withUser(DEVOPS) { inventory ->
             inventory.provision {
                 task actions: {
@@ -72,7 +144,7 @@ class ReplaceLineActionTest extends InventoryAwareTestBase {
     }
 
     @Test
-    void replaceLineChangeAttributes() {
+    void "replace line and change file attributes"() {
         withUser(DEVOPS) { inventory ->
             inventory.provision {
                 task actions: {
