4032ea556e4ca3e1d091e69d576f8aca32cf07c6
tiurikov <stanislav.tyurikov@gmail.com>
2018-07-14 15:50:48 +0200

    update upload folder tests

--- infrastructor-core/src/test/java/io/infrastructor/core/provisioning/actions/UploadFolderTest.groovy
+++ infrastructor-core/src/test/java/io/infrastructor/core/provisioning/actions/UploadFolderTest.groovy
@@ -1,12 +1,13 @@
 package io.infrastructor.core.provisioning.actions
 
 import io.infrastructor.core.inventory.InventoryAwareTestBase
+import io.infrastructor.core.provisioning.TaskExecutionException
 import org.junit.Test
 
 class UploadFolderTest extends InventoryAwareTestBase {
 
     @Test
-    void uploadFolderContentToRemoteHost() {
+    void "upload a folder to a remote host"() {
         withUser(DEVOPS) { inventory ->
             inventory.provision {
                 task actions: {
@@ -29,9 +30,52 @@ class UploadFolderTest extends InventoryAwareTestBase {
             }
         }
     }
-    
+
+
     @Test
-    void uploadFolderContentToRemoteHostWithIncludes() {
+    void "upload a folder to a remote host with sudo and a password"() {
+        withUser(SUDOPS) { inventory ->
+            inventory.provision {
+                task actions: {
+                    def result = upload {
+                        user = 'root'
+                        source = 'build/resources/test/upload'
+                        target = '/opt/test'
+                        sudopass = 'sudops'
+                    }
+
+                    assert result.exitcode == 0
+
+                    shell("ls /opt/test/").output.with {
+                        assert find(/file.exta/) && find(/file.extb/)
+                    }
+
+                    shell("ls /opt/test/nested").output.with {
+                        assert find(/file.exta/) && find(/file.extb/)
+                    }
+                }
+            }
+        }
+    }
+
+    @Test(expected = TaskExecutionException)
+    void "upload a folder to a remote host with sudo and a wrong password"() {
+        withUser(SUDOPS) { inventory ->
+            inventory.provision {
+                task actions: {
+                    def result = upload {
+                        user = 'root'
+                        source = 'build/resources/test/upload'
+                        target = '/opt/test'
+                        sudopass = 'wrong'
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    void "upload a folder to a remote host with includes"() {
         withUser(DEVOPS) { inventory ->
             inventory.provision {
                 task actions: {
@@ -53,7 +97,7 @@ class UploadFolderTest extends InventoryAwareTestBase {
     }
     
     @Test
-    void uploadFolderContentToRemoteHostWithIncludesNested() {
+    void "upload a folder to a remote host with nested includes"() {
         withUser(DEVOPS) { inventory ->
             inventory.provision {
                 task actions: {
@@ -79,7 +123,7 @@ class UploadFolderTest extends InventoryAwareTestBase {
     }
     
     @Test
-    void uploadFolderContentToRemoteHostWithIncludesNestedMultiple() {
+    void "upload a folder to a remote host with multiple nested includes"() {
         withUser(DEVOPS) { inventory ->
             inventory.provision {
                 task actions: {
@@ -105,7 +149,7 @@ class UploadFolderTest extends InventoryAwareTestBase {
     }
     
     @Test
-    void uploadFolderContentToRemoteHostWithExcludes() {
+    void "upload a folder to a remote host with excludes and includes"() {
         withUser(DEVOPS) { inventory ->
             inventory.provision {
                 task actions: {
@@ -132,7 +176,7 @@ class UploadFolderTest extends InventoryAwareTestBase {
     }
     
     @Test
-    void uploadFolderContentToRemoteHostWithExcludesOnly() {
+    void "upload a folder to a remote host with excludes only"() {
         withUser(DEVOPS) { inventory ->
             inventory.provision {
                 task actions: {
